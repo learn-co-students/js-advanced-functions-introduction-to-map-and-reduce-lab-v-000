f0a339a449bb823c238018923080ba55a51abc40
NaKia Whitby <whitby.nakia@gmail.com>
2019-12-16 21:34:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+  let r = [];
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    r.push(-1 * sourceArray[i]);
+  }
+  return r;
+}
+
+function mapToNoChange(sourceArray) {
+  let r = [];
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    r.push(sourceArray[i])
+  }
+  return r;
+}
+
+function mapToDouble(sourceArray) {
+  let r = [];
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    r.push(2 * sourceArray[i])
+  }
+  return r;
+}
+
+function mapToSquare(sourceArray) {
+  let r = [];
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    r.push(sourceArray[i]**2)
+  }
+  return r;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let r = startingPoint;
+
+  for (let i = 0; i < sourceArray.length; i++) {
+    r += sourceArray[i]
+  }
+  return r;
+}
+
+function reduceToAllTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if(!sourceArray[i]) {
+      return false
+      }
+  }
+  return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if(sourceArray[i]) {
+      return true
+      }
+  }
+  return false
+}
