a439d66b463fdc4a91617e13f473e342848e0524
ElvinGarcia <dimix181@gmail.com>
2020-03-10 10:56:51 -0400

    map-like methods completed

--- index.js
+++ index.js
@@ -12,13 +12,22 @@ function mapToSomething(x){
     }
 }
 
+function mapToExponent(x) {
+    return (sourceArray) => {
+        for (let i = 0; i < sourceArray.length; i++) {
+            sourceArray[i] = sourceArray[i] ** x;
+        }
+        return sourceArray
+    }
+}
+
 
 const mapToNegativize = mapToSomething(-1) 
 
 // suppose to return the array that was passed
 const mapToNoChange = mapToSomething(1)
 const mapToDouble = mapToSomething(2)
-const mapToSquare = mapToSomething(Math.pow(2, 2))
+const mapToSquare = mapToExponent(2)
 
 
 
