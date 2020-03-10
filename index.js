c105f83e9997087562a9f64f24568aa9c51a3eb9
ElvinGarcia <dimix181@gmail.com>
2020-03-09 22:16:33 -0400

    map to square needs refractoring

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+// map-like
+
+
+function mapToSomething(x){
+    return (sourceArray)=>{
+    for (let i = 0; i < sourceArray.length; i++) {
+        sourceArray[i] = sourceArray[i] * x;
+    }
+    return sourceArray
+    }
+}
+
+
+const mapToNegativize = mapToSomething(-1) 
+
+// suppose to return the array that was passed
+const mapToNoChange = mapToSomething(1)
+const mapToDouble = mapToSomething(2)
+const mapToSquare = mapToSomething(Math.pow(2, 2))
+
+
+
+
+
+
+
+// reduce-like
+
+
+function reduceToTotal(sourceArray, startingPoint){
+
+}
+function reduceToAllTrue(sourceArray){
+
+}
+function reduceToAnyTrue(sourceArray){
+
+}
+
