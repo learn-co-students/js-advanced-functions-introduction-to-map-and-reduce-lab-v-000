6063e834b15c12d5cd5d0637af1021b1eabbc796
aljimenez5 <arvida.jimenez@gmail.com>
2020-04-22 19:03:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    for (var i = 0; sourceArray.length > i; i++) {
+        sourceArray[i] *= -1
+    }
+    return sourceArray
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+    for (var i = 0; sourceArray.length > i; i++) {
+        sourceArray[i] *= 2
+    }
+    return sourceArray
+}
+
+function mapToSquare(sourceArray) {
+    for (var i = 0; sourceArray.length > i; i++) {
+        sourceArray[i] *= sourceArray[i]
+    }
+    return sourceArray
+}
+
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    var total = startingPoint
+    for (var i = 0; sourceArray.length > i; i++) {
+        total += sourceArray[i]
+    }
+    return total 
+
+}
+function reduceToAllTrue(sourceArray) {
+    var truthyValue = true
+    for (var i = 0; sourceArray.length > i; i++) {
+        if (sourceArray[i] === false) {
+            truthyValue = false
+        }
+    }
+    return truthyValue
+
+}
+function reduceToAnyTrue(sourceArray) {
+    var truthyValue = false
+    for (var i = 0; sourceArray.length > i; i++) {
+        if (sourceArray[i] === true) {
+            truthyValue = true
+        }
+    }
+    return truthyValue
+
+}
