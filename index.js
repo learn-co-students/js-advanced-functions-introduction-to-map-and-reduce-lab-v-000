55ae73847aa275c2233653373d9373088b0cd84c
Eva Navarro <emanao@me.com>
2020-07-15 17:50:10 +0000

    Map methods - Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function mapToNegativize(ary){
+  return ary.map(cur=>-Number(cur));
+}
+function mapToNoChange(ary){
+  return ary.map(cur=>cur);
+}
+function mapToDouble(ary){
+  return ary.map(cur=>2*Number(cur));
+}
+function mapToSquare(ary){
+  return ary.map(cur=>Math.pow(Number(cur),2));
+}
