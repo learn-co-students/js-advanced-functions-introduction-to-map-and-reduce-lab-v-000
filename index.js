70034bd6b9f19c2ef20635b3464a304bd9e4d8bf
Eva Navarro <emanao@me.com>
2020-07-15 18:39:34 +0000

    Map-like functions

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
+// function mapToNegativize(ary){
+//   return ary.map(cur=>-Number(cur));
+// }
 function mapToNegativize(ary){
-  return ary.map(cur=>-Number(cur));
+  const new_ary = []
+  ary.forEach(elem=>new_ary.push(-Number(elem)));
+  return new_ary;
 }
+
+// function mapToNoChange(ary){
+//   return ary.map(cur=>cur);
+// }
 function mapToNoChange(ary){
-  return ary.map(cur=>cur);
+  const new_ary = []
+  ary.forEach(elem=>new_ary.push(elem));
+  return new_ary;
 }
+
+// function mapToDouble(ary){
+//   return ary.map(cur=>2*Number(cur));
+// }
 function mapToDouble(ary){
-  return ary.map(cur=>2*Number(cur));
+  const new_ary = []
+  ary.forEach(elem=>new_ary.push(2*Number(elem)));
+  return new_ary;
 }
+
+// function mapToSquare(ary){
+//   return ary.map(cur=>Math.pow(Number(cur),2));
+// }
 function mapToSquare(ary){
-  return ary.map(cur=>Math.pow(Number(cur),2));
+  const new_ary = []
+  ary.forEach(elem=>new_ary.push(Math.pow(Number(elem),2)));
+  return new_ary;
 }
+
 function reduceToTotal(ary, initialValue){
   return ary.reduce( (acc, cur)=> acc+cur, !!initialValue?initialValue:0 );
 }
