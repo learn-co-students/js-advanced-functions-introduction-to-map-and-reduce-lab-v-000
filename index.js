f15769a4c9d184134a5eaae81df5e669d0744674
Eva Navarro <emanao@me.com>
2020-07-15 18:24:49 +0000

    Reduce methods - Done

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function mapToDouble(ary){
 function mapToSquare(ary){
   return ary.map(cur=>Math.pow(Number(cur),2));
 }
+function reduceToTotal(ary, initialValue){
+  return ary.reduce( (acc, cur)=> acc+cur, !!initialValue?initialValue:0 );
+}
+
+function reduceToAllTrue(ary){
+  return ary.reduce((acc, cur)=>!!acc&&!!cur)
+}
+function reduceToAnyTrue(ary){
+  return ary.reduce((acc, cur)=>!!acc||!!cur)
+}
