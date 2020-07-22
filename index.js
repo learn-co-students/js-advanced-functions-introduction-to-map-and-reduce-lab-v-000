ff8e1788a8f32f80ba09ebfd30e006c6950d5e06
github username <github email address>
2020-07-22 13:12:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,47 +1,71 @@
-// Your code here
-function mapToNegativize(sourceArray) {
-    return sourceArray.map(x => x*-1)
-}
-
-function mapToNoChange(sourceArray) {
-    return sourceArray.map(x => x)
-}
-
-function mapToDouble(sourceArray) {
-    return sourceArray.map(x => x * 2)
-}
-
-function mapToSquare(sourceArray) {
-    return sourceArray.map(x => x * x)
-}
-
-function reduceToTotal(sourceArray, startingPoint=0) {
-    const reducer = function(accumulator, startingPoint) {
-        return (accumulator + startingPoint)
-    };
-    return sourceArray.reduce(reducer, startingPoint);
-}
-
-function reduceToAllTrue(sourceArray) {
-    const reducer = function(accumulator, currentValue) {
-        if(!!accumulator == true && !!currentValue == true) {
-            return true
-        } 
-        else {
-            return !!false
-        }
+function mapToNegativize(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(-1 * src[i]) // Unique work
     }
-    return sourceArray.reduce(reducer, true)
+    return r
+  }
+   
+  function mapToNoChange(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(src[i]) // Unique work
+    }
+    return r
+  }
+   
+  function mapToDouble(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(2 * src[i]) // Unique work
+    }
+    return r
+  }
+   
+  function mapToSquare(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(src[i] * src[i]) // Unique work
+    }
+    return r
+  }
+
+// function reduceToTotal(sourceArray, startingPoint=0) {
+//     const reducer = function(accumulator, startingPoint) {
+//         return (accumulator + startingPoint)
+//     };
+//     return sourceArray.reduce(reducer, startingPoint);
+// }
+
+function reduceToTotal(sourceArray, startingPoint) {
+    let startingPoint = 0
+
+    for(src of sourceArray) {
+        i += i 
+    }
+    return startingPoint + i
 }
 
-function reduceToAnyTrue(sourceArray) {
-    const reducer = function(accumulator, currentValue) {
-        if(accumulator == true) {
-            return true
-        }
-        else {
-                return !!currentValue
-        }
-    }
-    return sourceArray.reduce(reducer, false)
-}
+// function reduceToAllTrue(sourceArray) {
+//     const reducer = function(accumulator, currentValue) {
+//         if(!!accumulator == true && !!currentValue == true) {
+//             return true
+//         } 
+//         else {
+//             return !!false
+//         }
+//     }
+//     return sourceArray.reduce(reducer, true)
+// }
+
+// function reduceToAnyTrue(sourceArray) {
+//     const reducer = function(accumulator, currentValue) {
+//         if(accumulator == true) {
+//             return true
+//         }
+//         else {
+//                 return !!currentValue
+//         }
+//     }
+//     return sourceArray.reduce(reducer, false)
+// }
