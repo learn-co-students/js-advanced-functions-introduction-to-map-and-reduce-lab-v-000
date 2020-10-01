f094e0acbb35a3ae045155070e5caa97fcebd4fd
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-01 18:25:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function mapToNegativize(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        r.push(-1 * src[i]) // Unique work
+    }
+    return r
+}
+
+function mapToNoChange(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        r.push(src[i]) // Unique work
+    }
+    return r
+}
+
+function mapToDouble(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        r.push(2 * src[i]) // Unique work
+    }
+    return r
+}
+
+function mapToSquare(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        r.push(src[i] * src[i]) // Unique work
+    }
+    return r
+}
+
+function reduceToTotal(src) {
+    // let t = []
+    for (let i = 0; i< src.length; i++) {
+    return i
+    }
+}
