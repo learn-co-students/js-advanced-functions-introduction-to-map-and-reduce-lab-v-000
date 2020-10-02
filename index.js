48725c5b48048e1a15824429329a510f4417009f
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-02 02:33:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,95 @@
 // Your code here
-const mapToNegativize = collection => collection.map(x => x * -1);
+
+//using map
+
+// const mapToNegativize = collection => collection.map(x => x * -1);
 // ES6 notation
 // if {} must include return
 // no {} = implicit return
 
-//same as above
-// function mapToNegativize(collection){
-//   return collection.map(function(x) {
-//     return x * -1
-//   }
+const mapToNegativize = collection => {
+  const newCollection = [];
+  collection.forEach(element => {
+    newCollection.push(element * -1);
+  });
+  return newCollection;
+};
+
+// const mapToNoChange = collection => collection.map(x => x);
+
+const mapToNoChange = collection => {
+   const newCollection = [];
+   collection.forEach(element => {
+     newCollection.push(element);
+   });
+   return newCollection;
+}
+
+// const mapToDouble = collection => collection.map(x => x * 2);
+
+const mapToDouble = collection => {
+  const newCollection = [];
+  collection.forEach(element => {
+    newCollection.push(element * 2);
+  });
+  return newCollection;
+}
+
+// const mapToSquare = collection => collection.map(x => x * x);
+
+const mapToSquare = collection => {
+  const newCollection = [];
+  collection.forEach(element => {
+    newCollection.push(element * element)
+  });
+  return newCollection;
+};
+
+//using reduce
+
+// const reduceToTotal = (collection, startingPoint = 0) => {
+//   return collection.reduce((total, value) => {
+//     return total + value;
+//   }, startingPoint);
 // }
 
-const mapToNoChange = collection => collection.map(x => x);
-
-const mapToDouble = collection => collection.map(x => x * 2);
-
-const mapToSquare = collection => collection.map(x => x * x);
-
 const reduceToTotal = (collection, startingPoint = 0) => {
-  return collection.reduce((total, value) => {
-    return total + value;
-  }, startingPoint);
+  let result = startingPoint;
+  collection.forEach(element => {
+    result = result + element
+  });
+  return result;
 }
 
-const reduceToAllTrue = (collection) => {
-  return collection.reduce((condition, element) => {
-    return condition && !!element;
-  }, true);
-}
+// const reduceToAllTrue = (collection) => {
+//   return collection.reduce((condition, element) => {
+//     return condition && !!element;
+//   }, true);
+// }
 
-const reduceToAnyTrue = (collection) => {
-  return collection.reduce((condition, element) => {
-    return condition || !!element;
-  }, false);
+const reduceToAllTrue = collection => {
+  let condition;
+  collection.forEach(element => {
+    if (!!element) {
+      condition = true;
+    } else {
+      condition = false;
+    };
+  });
+  return condition;
+}
+//
+// const reduceToAnyTrue = (collection) => {
+//   return collection.reduce((condition, element) => {
+//     return condition || !!element;
+//   }, false);
+// }
+
+const reduceToAnyTrue = collection => {
+  for( const element of collection) {
+    if (!!element) {
+      return true;
+    };
+  };
+  return false;
 }
