995fcd9912015ca70ee895a86595314ffda547ff
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2021-05-04 00:11:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+  return sourceArray.map( x => x * -1 )
+}
+
+const mapToNoChange = (sourceArray) => {
+  return sourceArray.map( x => x )
+}
+
+const mapToDouble = (sourceArray) => {
+  return sourceArray.map( x => x * 2 )
+}
+
+const mapToSquare = (sourceArray) => {
+  return sourceArray.map( x => x * x )
+}
+
+const reduceToTotal = (sourceArray, startingPoint = 0) => {
+  return sourceArray.reduce((sum, value) => sum + value, startingPoint)
+}
+
+const reduceToAllTrue = (sourceArray) => {
+  return sourceArray.reduce((a,b) => a && b ? true : false)
+}
+
+const reduceToAnyTrue = (sourceArray) => {
+  return sourceArray.reduce((a,b) => a || b ? true : false)
+}
