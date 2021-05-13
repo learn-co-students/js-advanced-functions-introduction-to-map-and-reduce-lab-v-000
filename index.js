860a1d4b388db2d4f7056456d835e490e1ad6e81
capricious-slingshot <ephemere@protonmail.com>
2021-05-13 15:05:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+//map
+function mapToNegativize(arr){
+  const newValues = []
+  for (let i of arr){ newValues.push(i * -1) }
+  return newValues
+}
+
+function mapToNoChange(arr){ return [...arr] }
+
+function mapToDouble(arr){
+  const newValues = []
+  for (let i of arr){ newValues.push(i * 2) }
+  return newValues
+}
+
+function mapToSquare(arr){
+  const newValues = []
+  for (let i of arr){ newValues.push(i * i) }
+  return newValues
+}
+
+
+//reduce
+function reduceToTotal(arr, value = 0){ 
+  for (let i of arr){ value += i }
+  return value
+}
+
+function reduceToAllTrue(arr){
+  for (let i of arr){ if (!i){ return false } }
+  return true
+}
+
+function reduceToAnyTrue(arr){
+  for (let i of arr){ if (i){ return true } }
+  return false
+}
+
+function mapToDouble(){
+
+}
+
